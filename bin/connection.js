ccdac00b961c996cc62a20503cf50ed70eef7e1e
moturimanideep <manideep.digischool@gmail.com>
2018-03-05 15:40:31 +0530

    Update connection.js
    Update of DB URL

--- bin/connection.js
+++ bin/connection.js
@@ -1,7 +1,7 @@
 
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-var url = 'http://127.0.0.1:27017/test';
+var url = 'mongodb://admin:admin@ds255258.mlab.com:55258/dlworkshop';
 module.exports.init = function(){
     MongoClient.connect(url, function(err, db){
         if(!err){
