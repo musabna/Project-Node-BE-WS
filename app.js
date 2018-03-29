c6383c9dd7b8684616a6e35791a478c476cb70b4
moturimanideep <manideep.digischool@gmail.com>
2018-03-29 18:26:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ mongoose.connect('mongodb://admin:admin@ds255258.mlab.com:55258/dlworkshop');
 
 var app = express();
 app.use(cors());
-app.use("*", function(req, res, next) {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'accept, Content-Type, Authorization');
+// app.use("*", function(req, res, next) {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'accept, Content-Type, Authorization');
+// });
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 //console.log(db);
 
