bcdf390607d8b1cf2d98a0371b0fd999c3057cc0
moturimanideep <manideep.digischool@gmail.com>
2018-03-29 18:11:28 +0530

    Update app.js
    Cors addition

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://admin:admin@ds255258.mlab.com:55258/dlworkshop');
 
 var app = express();
 app.use(cors());
-pp.use(function(req, res, next) {
+app.use(function(req, res, next) {
  res.header(“Access-Control-Allow-Origin”, “*”);
  res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept”);
  next();
